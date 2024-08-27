a83ec387daec1f7870f7dfa1b83ce7e06544bd05
SheryarAmir <dev.sheryaramir@gmail.com>
2024-08-27 20:10:35 +0500

    lerarning TypeScript

---
+++ TypeScript/index.ts
@@ -0,0 +1,28 @@
+function Add(a:number,b:number):number{
+  return a+b
+}
+console.log(Add(10,234))
+
+//type annotations : in typescript type annotation is a way of  explicitly specifying the type of variable .function parameters,or function return value.type annotations are expressed by using a colon(:) followed by desired type after the varible or function parameter declaration. there can be a space after the colon.
+
+
+
+let longsen:string="this is is sheryar amir "
+let shortsen:string=longsen.slice(10)
+console.log(shortsen)
+
+
+let sen:string="allowUnreachableCode,allowUnusedLabels,alwaysStrict,exactOptionalPropertyTypes,"
+let sen1:string="allowUnreachableCode,allowUnusedLabels,alwaysStrict,exactOptionalPropertyTypes,"
+
+if(sen===sen1){
+  console.log("both are equal")
+
+}
+else{
+  console.log("different")
+}
+
+
+let isdone:boolean=true;
+
