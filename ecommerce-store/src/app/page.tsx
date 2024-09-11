0e4d73244daf0cb77f2b343ac2d92e993536b7bd
SheryarAmir <dev.sheryaramir@gmail.com>
2024-09-11 23:41:40 +0500

    creating ecommerce store

---
+++ ecommerce-store/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main className="text-black">
+    <h1 className=" text-3xl">this is the ecommerce app</h1>
+   </main>
+  );
+}
